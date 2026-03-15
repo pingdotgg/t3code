5378fcde51d52b22b29d94aa67ed9e1e06b34b7c
Noah Gregory <noah@wts.dev>
2026-03-15 00:18:28 -0400

    fix: only init the current platform for native module

--- packages/native/src/native/native.cpp
+++ packages/native/src/native/native.cpp
@@ -1,6 +1,12 @@
+#include "platforms/platform.h"
+
+#ifdef T3TOOLS_OS_MACOS
 #include "platforms/darwin.h"
+#elif T3TOOLS_OS_LINUX
 #include "platforms/linux.h"
+#elif T3TOOLS_OS_WINDOWS
 #include "platforms/win32.h"
+#endif
 #include <napi.h>
 
 namespace t3tools::native {
@@ -11,9 +17,13 @@ Napi::Value Ping(const Napi::CallbackInfo &info) {
 }
 
 static Napi::Object Init(Napi::Env env, Napi::Object exports) {
+  #ifdef T3TOOLS_OS_MACOS
   platforms::darwin::Init(env, exports);
+  #elif T3TOOLS_OS_LINUX
   platforms::gnu_linux::Init(env, exports);
+  #elif T3TOOLS_OS_WINDOWS
   platforms::win32::Init(env, exports);
+  #endif
   exports.Set("ping", Napi::Function::New(env, Ping));
   return exports;
 }
