tmacc/agentdynamo

Add ACP support with Cursor provider (#1355)
Co-authored-by: codex <codex@users.noreply.github.com>
Co-authored-by: Julius Marminge <julius@macmini.local>
Co-authored-by: Cursor Agent <cursoragent@cursor.com>
Co-authored-by: cursor[bot] <206951365+cursor[bot]@users.noreply.github.com>

apps/desktop/tsdown.config.ts
@@ -12,7 +12,7 @@ export default defineConfig([
     ...shared,
     entry: ["src/main.ts"],
     clean: true,
-    noExternal: (id) => id.startsWith("@t3tools/"),
+    noExternal: (id) => id.startsWith("@t3tools/") || id.startsWith("effect-acp"),
   },
   {
     ...shared,

apps/server/src/provider/Layers/ProviderAdapterRegistry.test.ts
@@ -8,6 +8,8 @@ import { ClaudeAdapter } from "../Services/ClaudeAdapter.ts";
 import type { ClaudeAdapterShape } from "../Services/ClaudeAdapter.ts";
 import { CodexAdapter } from "../Services/CodexAdapter.ts";
 import type { CodexAdapterShape } from "../Services/CodexAdapter.ts";
+import { CursorAdapter } from "../Services/CursorAdapter.ts";
+import type { CursorAdapterShape } from "../Services/CursorAdapter.ts";
 import { OpenCodeAdapter } from "../Services/OpenCodeAdapter.ts";
 import type { OpenCodeAdapterShape } from "../Services/OpenCodeAdapter.ts";
 import { ProviderAdapterRegistry } from "../Services/ProviderAdapterRegistry.ts";
@@ -66,6 +68,23 @@ const fakeOpenCodeAdapter: OpenCodeAdapterShape = {
   streamEvents: Stream.empty,
 };
 
+const fakeCursorAdapter: CursorAdapterShape = {
+  provider: "cursor",
+  capabilities: { sessionModelSwitch: "in-session" },
+  startSession: vi.fn(),
+  sendTurn: vi.fn(),
+  interruptTurn: vi.fn(),
+  respondToRequest: vi.fn(),
+  respondToUserInput: vi.fn(),
+  stopSession: vi.fn(),
+  listSessions: vi.fn(),
+  hasSession: vi.fn(),
+  readThread: vi.fn(),
+  rollbackThread: vi.fn(),
+  stopAll: vi.fn(),
+  streamEvents: Stream.empty,
+};
+
 const layer = it.layer(
   Layer.mergeAll(
     Layer.provide(
@@ -74,6 +93,7 @@ const layer = it.layer(
         Layer.succeed(CodexAdapter, fakeCodexAdapter),
         Layer.succeed(ClaudeAdapter, fakeClaudeAdapter),
         Layer.succeed(OpenCodeAdapter, fakeOpenCodeAdapter),
+        Layer.succeed(CursorAdapter, fakeCursorAdapter),
       ),
     ),
     NodeServices.layer,
@@ -87,12 +107,14 @@ layer("ProviderAdapterRegistryLive", (it) => {
       const codex = yield* registry.getByProvider("codex");
       const claude = yield* registry.getByProvider("claudeAgent");
       const openCode = yield* registry.getByProvider("opencode");
+      const cursor = yield* registry.getByProvider("cursor");
       assert.equal(codex, fakeCodexAdapter);
       assert.equal(claude, fakeClaudeAdapter);
       assert.equal(openCode, fakeOpenCodeAdapter);
+      assert.equal(cursor, fakeCursorAdapter);
 
       const providers = yield* registry.listProviders();
-      assert.deepEqual(providers, ["codex", "claudeAgent", "opencode"]);
+      assert.deepEqual(providers, ["codex", "claudeAgent", "opencode", "cursor"]);
     }),
   );
 

apps/server/src/provider/acp/AcpCoreRuntimeEvents.test.ts
@@ -1,0 +1,155 @@
+import { RuntimeRequestId, TurnId } from "@t3tools/contracts";
+import { describe, expect, it } from "vitest";
+
+import {
+  makeAcpAssistantItemEvent,
+  makeAcpContentDeltaEvent,
+  makeAcpPlanUpdatedEvent,
+  makeAcpRequestOpenedEvent,
+  makeAcpRequestResolvedEvent,
+  makeAcpToolCallEvent,
+} from "./AcpCoreRuntimeEvents.ts";
+
+describe("AcpCoreRuntimeEvents", () => {
+  it("maps ACP permission requests to canonical runtime events", () => {
+    const stamp = { eventId: "event-1" as never, createdAt: "2026-03-27T00:00:00.000Z" };
+    const turnId = TurnId.make("turn-1");
+    const permissionRequest = {
+      kind: "execute" as const,
+      detail: "cat package.json",
+      toolCall: {
+        toolCallId: "tool-1",
+        kind: "execute",
+        status: "pending" as const,
+        command: "cat package.json",
+        detail: "cat package.json",
+        data: { toolCallId: "tool-1", kind: "execute" },
+      },
+    };
+
+    expect(
+      makeAcpRequestOpenedEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        requestId: RuntimeRequestId.make("request-1"),
+        permissionRequest,
+        detail: "cat package.json",
+        args: { command: ["cat", "package.json"] },
+        source: "acp.jsonrpc",
+        method: "session/request_permission",
+        rawPayload: { sessionId: "session-1" },
+      }),
+    ).toMatchObject({
+      type: "request.opened",
+      payload: {
+        requestType: "exec_command_approval",
+        detail: "cat package.json",
+      },
+    });
+
+    expect(
+      makeAcpRequestResolvedEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        requestId: RuntimeRequestId.make("request-1"),
+        permissionRequest,
+        decision: "accept",
+      }),
+    ).toMatchObject({
+      type: "request.resolved",
+      payload: {
+        requestType: "exec_command_approval",
+        decision: "accept",
+      },
+    });
+  });
+
+  it("maps ACP core plan, tool-call, and content updates", () => {
+    const stamp = { eventId: "event-1" as never, createdAt: "2026-03-27T00:00:00.000Z" };
+    const turnId = TurnId.make("turn-1");
+
+    expect(
+      makeAcpPlanUpdatedEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        payload: {
+          plan: [{ step: "Inspect state", status: "inProgress" }],
+        },
+        source: "acp.cursor.extension",
+        method: "cursor/update_todos",
+        rawPayload: { todos: [] },
+      }),
+    ).toMatchObject({
+      type: "turn.plan.updated",
+      raw: {
+        method: "cursor/update_todos",
+      },
+    });
+
+    expect(
+      makeAcpToolCallEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        toolCall: {
+          toolCallId: "tool-1",
+          kind: "execute",
+          status: "completed",
+          title: "Terminal",
+          detail: "bun run test",
+          data: { command: "bun run test" },
+        },
+        rawPayload: { sessionId: "session-1" },
+      }),
+    ).toMatchObject({
+      type: "item.completed",
+      payload: {
+        itemType: "command_execution",
+        status: "completed",
+      },
+    });
+
+    expect(
+      makeAcpContentDeltaEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        itemId: "assistant:session-1:segment:0",
+        text: "hello",
+        rawPayload: { sessionId: "session-1" },
+      }),
+    ).toMatchObject({
+      type: "content.delta",
+      itemId: "assistant:session-1:segment:0",
+      payload: {
+        delta: "hello",
+      },
+    });
+
+    expect(
+      makeAcpAssistantItemEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        itemId: "assistant:session-1:segment:0",
+        lifecycle: "item.started",
+      }),
+    ).toMatchObject({
+      type: "item.started",
+      itemId: "assistant:session-1:segment:0",
+      payload: {
+        itemType: "assistant_message",
+        status: "inProgress",
+      },
+    });
+  });
+});

apps/server/src/provider/acp/AcpNativeLogging.ts
@@ -1,0 +1,76 @@
+import type { ProviderKind, ThreadId } from "@t3tools/contracts";
+import { Cause, Effect } from "effect";
+import type * as EffectAcpProtocol from "effect-acp/protocol";
+
+import type { EventNdjsonLogger } from "../Layers/EventNdjsonLogger.ts";
+import type { AcpSessionRequestLogEvent, AcpSessionRuntimeOptions } from "./AcpSessionRuntime.ts";
+
+function writeNativeAcpLog(input: {
+  readonly nativeEventLogger: EventNdjsonLogger | undefined;
+  readonly provider: ProviderKind;
+  readonly threadId: ThreadId;
+  readonly kind: "request" | "protocol";
+  readonly payload: unknown;
+}): Effect.Effect<void, never> {
+  return Effect.gen(function* () {
+    if (!input.nativeEventLogger) return;
+    const observedAt = new Date().toISOString();
+    yield* input.nativeEventLogger.write(
+      {
+        observedAt,
+        event: {
+          id: crypto.randomUUID(),
+          kind: input.kind,
+          provider: input.provider,
+          createdAt: observedAt,
+          threadId: input.threadId,
+          payload: input.payload,
+        },
+      },
+      input.threadId,
+    );
+  });
+}
+
+function formatRequestLogPayload(event: AcpSessionRequestLogEvent) {
+  return {
+    method: event.method,
+    status: event.status,
+    request: event.payload,
+    ...(event.result !== undefined ? { result: event.result } : {}),
+    ...(event.cause !== undefined ? { cause: Cause.pretty(event.cause) } : {}),
+  };
+}
+
+export function makeAcpNativeLoggers(input: {
+  readonly nativeEventLogger: EventNdjsonLogger | undefined;
+  readonly provider: ProviderKind;
+  readonly threadId: ThreadId;
+}): Pick<AcpSessionRuntimeOptions, "requestLogger" | "protocolLogging"> {
+  return {
+    requestLogger: (event) =>
+      writeNativeAcpLog({
+        nativeEventLogger: input.nativeEventLogger,
+        provider: input.provider,
+        threadId: input.threadId,
+        kind: "request",
+        payload: formatRequestLogPayload(event),
+      }),
+    ...(input.nativeEventLogger
+      ? {
+          protocolLogging: {
+            logIncoming: true,
+            logOutgoing: true,
+            logger: (event: EffectAcpProtocol.AcpProtocolLogEvent) =>
+              writeNativeAcpLog({
+                nativeEventLogger: input.nativeEventLogger,
+                provider: input.provider,
+                threadId: input.threadId,
+                kind: "protocol",
+                payload: event,
+              }),
+          } satisfies NonNullable<AcpSessionRuntimeOptions["protocolLogging"]>,
+        }
+      : {}),
+  };
+}

pingdotgg/t3code

apps/desktop/tsdown.config.ts
@@ -20,7 +20,7 @@ export default defineConfig([
     ...shared,
     entry: ["src/main.ts"],
     clean: true,
-    noExternal: (id) => id.startsWith("@marcode/"),
+    noExternal: (id) => id.startsWith("@marcode/") || id.startsWith("effect-acp"),
     define: embeddedJiraDefines,
   },
   {

apps/server/src/provider/Layers/ProviderAdapterRegistry.test.ts
@@ -8,6 +8,8 @@ import { ClaudeAdapter } from "../Services/ClaudeAdapter.ts";
 import type { ClaudeAdapterShape } from "../Services/ClaudeAdapter.ts";
 import { CodexAdapter } from "../Services/CodexAdapter.ts";
 import type { CodexAdapterShape } from "../Services/CodexAdapter.ts";
+import { CursorAdapter } from "../Services/CursorAdapter.ts";
+import type { CursorAdapterShape } from "../Services/CursorAdapter.ts";
 import { OpenCodeAdapter } from "../Services/OpenCodeAdapter.ts";
 import type { OpenCodeAdapterShape } from "../Services/OpenCodeAdapter.ts";
 import { ProviderAdapterRegistry } from "../Services/ProviderAdapterRegistry.ts";
@@ -66,6 +68,23 @@ const fakeOpenCodeAdapter: OpenCodeAdapterShape = {
   streamEvents: Stream.empty,
 };
 
+const fakeCursorAdapter: CursorAdapterShape = {
+  provider: "cursor",
+  capabilities: { sessionModelSwitch: "in-session" },
+  startSession: vi.fn(),
+  sendTurn: vi.fn(),
+  interruptTurn: vi.fn(),
+  respondToRequest: vi.fn(),
+  respondToUserInput: vi.fn(),
+  stopSession: vi.fn(),
+  listSessions: vi.fn(),
+  hasSession: vi.fn(),
+  readThread: vi.fn(),
+  rollbackThread: vi.fn(),
+  stopAll: vi.fn(),
+  streamEvents: Stream.empty,
+};
+
 const layer = it.layer(
   Layer.mergeAll(
     Layer.provide(
@@ -74,6 +93,7 @@ const layer = it.layer(
         Layer.succeed(CodexAdapter, fakeCodexAdapter),
         Layer.succeed(ClaudeAdapter, fakeClaudeAdapter),
         Layer.succeed(OpenCodeAdapter, fakeOpenCodeAdapter),
+        Layer.succeed(CursorAdapter, fakeCursorAdapter),
       ),
     ),
     NodeServices.layer,
@@ -87,12 +107,14 @@ layer("ProviderAdapterRegistryLive", (it) => {
       const codex = yield* registry.getByProvider("codex");
       const claude = yield* registry.getByProvider("claudeAgent");
       const openCode = yield* registry.getByProvider("opencode");
+      const cursor = yield* registry.getByProvider("cursor");
       assert.equal(codex, fakeCodexAdapter);
       assert.equal(claude, fakeClaudeAdapter);
       assert.equal(openCode, fakeOpenCodeAdapter);
+      assert.equal(cursor, fakeCursorAdapter);
 
       const providers = yield* registry.listProviders();
-      assert.deepEqual(providers, ["claudeAgent", "codex", "opencode"]);
+      assert.deepEqual(providers, ["claudeAgent", "codex", "opencode", "cursor"]);
     }),
   );
 

apps/server/src/provider/acp/AcpCoreRuntimeEvents.test.ts
@@ -1,0 +1,155 @@
+import { RuntimeRequestId, TurnId } from "@marcode/contracts";
+import { describe, expect, it } from "vitest";
+
+import {
+  makeAcpAssistantItemEvent,
+  makeAcpContentDeltaEvent,
+  makeAcpPlanUpdatedEvent,
+  makeAcpRequestOpenedEvent,
+  makeAcpRequestResolvedEvent,
+  makeAcpToolCallEvent,
+} from "./AcpCoreRuntimeEvents.ts";
+
+describe("AcpCoreRuntimeEvents", () => {
+  it("maps ACP permission requests to canonical runtime events", () => {
+    const stamp = { eventId: "event-1" as never, createdAt: "2026-03-27T00:00:00.000Z" };
+    const turnId = TurnId.make("turn-1");
+    const permissionRequest = {
+      kind: "execute" as const,
+      detail: "cat package.json",
+      toolCall: {
+        toolCallId: "tool-1",
+        kind: "execute",
+        status: "pending" as const,
+        command: "cat package.json",
+        detail: "cat package.json",
+        data: { toolCallId: "tool-1", kind: "execute" },
+      },
+    };
+
+    expect(
+      makeAcpRequestOpenedEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        requestId: RuntimeRequestId.make("request-1"),
+        permissionRequest,
+        detail: "cat package.json",
+        args: { command: ["cat", "package.json"] },
+        source: "acp.jsonrpc",
+        method: "session/request_permission",
+        rawPayload: { sessionId: "session-1" },
+      }),
+    ).toMatchObject({
+      type: "request.opened",
+      payload: {
+        requestType: "exec_command_approval",
+        detail: "cat package.json",
+      },
+    });
+
+    expect(
+      makeAcpRequestResolvedEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        requestId: RuntimeRequestId.make("request-1"),
+        permissionRequest,
+        decision: "accept",
+      }),
+    ).toMatchObject({
+      type: "request.resolved",
+      payload: {
+        requestType: "exec_command_approval",
+        decision: "accept",
+      },
+    });
+  });
+
+  it("maps ACP core plan, tool-call, and content updates", () => {
+    const stamp = { eventId: "event-1" as never, createdAt: "2026-03-27T00:00:00.000Z" };
+    const turnId = TurnId.make("turn-1");
+
+    expect(
+      makeAcpPlanUpdatedEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        payload: {
+          plan: [{ step: "Inspect state", status: "inProgress" }],
+        },
+        source: "acp.cursor.extension",
+        method: "cursor/update_todos",
+        rawPayload: { todos: [] },
+      }),
+    ).toMatchObject({
+      type: "turn.plan.updated",
+      raw: {
+        method: "cursor/update_todos",
+      },
+    });
+
+    expect(
+      makeAcpToolCallEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        toolCall: {
+          toolCallId: "tool-1",
+          kind: "execute",
+          status: "completed",
+          title: "Terminal",
+          detail: "bun run test",
+          data: { command: "bun run test" },
+        },
+        rawPayload: { sessionId: "session-1" },
+      }),
+    ).toMatchObject({
+      type: "item.completed",
+      payload: {
+        itemType: "command_execution",
+        status: "completed",
+      },
+    });
+
+    expect(
+      makeAcpContentDeltaEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        itemId: "assistant:session-1:segment:0",
+        text: "hello",
+        rawPayload: { sessionId: "session-1" },
+      }),
+    ).toMatchObject({
+      type: "content.delta",
+      itemId: "assistant:session-1:segment:0",
+      payload: {
+        delta: "hello",
+      },
+    });
+
+    expect(
+      makeAcpAssistantItemEvent({
+        stamp,
+        provider: "cursor",
+        threadId: "thread-1" as never,
+        turnId,
+        itemId: "assistant:session-1:segment:0",
+        lifecycle: "item.started",
+      }),
+    ).toMatchObject({
+      type: "item.started",
+      itemId: "assistant:session-1:segment:0",
+      payload: {
+        itemType: "assistant_message",
+        status: "inProgress",
+      },
+    });
+  });
+});

apps/server/src/provider/acp/AcpNativeLogging.ts
@@ -1,0 +1,76 @@
+import type { ProviderKind, ThreadId } from "@marcode/contracts";
+import { Cause, Effect } from "effect";
+import type * as EffectAcpProtocol from "effect-acp/protocol";
+
+import type { EventNdjsonLogger } from "../Layers/EventNdjsonLogger.ts";
+import type { AcpSessionRequestLogEvent, AcpSessionRuntimeOptions } from "./AcpSessionRuntime.ts";
+
+function writeNativeAcpLog(input: {
+  readonly nativeEventLogger: EventNdjsonLogger | undefined;
+  readonly provider: ProviderKind;
+  readonly threadId: ThreadId;
+  readonly kind: "request" | "protocol";
+  readonly payload: unknown;
+}): Effect.Effect<void, never> {
+  return Effect.gen(function* () {
+    if (!input.nativeEventLogger) return;
+    const observedAt = new Date().toISOString();
+    yield* input.nativeEventLogger.write(
+      {
+        observedAt,
+        event: {
+          id: crypto.randomUUID(),
+          kind: input.kind,
+          provider: input.provider,
+          createdAt: observedAt,
+          threadId: input.threadId,
+          payload: input.payload,
+        },
+      },
+      input.threadId,
+    );
+  });
+}
+
+function formatRequestLogPayload(event: AcpSessionRequestLogEvent) {
+  return {
+    method: event.method,
+    status: event.status,
+    request: event.payload,
+    ...(event.result !== undefined ? { result: event.result } : {}),
+    ...(event.cause !== undefined ? { cause: Cause.pretty(event.cause) } : {}),
+  };
+}
+
+export function makeAcpNativeLoggers(input: {
+  readonly nativeEventLogger: EventNdjsonLogger | undefined;
+  readonly provider: ProviderKind;
+  readonly threadId: ThreadId;
+}): Pick<AcpSessionRuntimeOptions, "requestLogger" | "protocolLogging"> {
+  return {
+    requestLogger: (event) =>
+      writeNativeAcpLog({
+        nativeEventLogger: input.nativeEventLogger,
+        provider: input.provider,
+        threadId: input.threadId,
+        kind: "request",
+        payload: formatRequestLogPayload(event),
+      }),
+    ...(input.nativeEventLogger
+      ? {
+          protocolLogging: {
+            logIncoming: true,
+            logOutgoing: true,
+            logger: (event: EffectAcpProtocol.AcpProtocolLogEvent) =>
+              writeNativeAcpLog({
+                nativeEventLogger: input.nativeEventLogger,
+                provider: input.provider,
+                threadId: input.threadId,
+                kind: "protocol",
+                payload: event,
+              }),
+          } satisfies NonNullable<AcpSessionRuntimeOptions["protocolLogging"]>,
+        }
+      : {}),
+  };
+}