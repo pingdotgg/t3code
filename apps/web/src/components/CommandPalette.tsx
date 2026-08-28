162a6df30106e92490801777354d7ba4b2cb1f42
Vaishnav S Menon <vaishnavsm@gmail.com>
2026-08-28 14:04:45 +0530

    fix(web): show the correct new thread shortcut in command palette

--- apps/web/src/components/CommandPalette.tsx
+++ apps/web/src/components/CommandPalette.tsx
@@ -1502,7 +1502,7 @@ function OpenCommandPaletteDialog(props: {
           </>
         ),
         icon: <SquarePenIcon className={ITEM_ICON_CLASS} />,
-        shortcutCommand: "chat.new",
+        shortcutCommand: "chat.newLocal",
         run: async () => {
           await startNewThreadFromContext({
             activeDraftThread,
