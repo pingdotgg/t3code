912dfe7a7a8111bf6f7071857bd8cc8515ffacb8
Neel <neelpatel805804@gmail.com>
2026-09-06 22:00:58 +0530

    fix(web): hide browser when the right panel starts closing

--- apps/web/src/components/ChatView.tsx
+++ apps/web/src/components/ChatView.tsx
@@ -7632,7 +7632,7 @@ export default function ChatView(props: ChatViewProps) {
           threadRef={activeThreadRef}
           tabId={renderedRightPanelSurface.resourceId}
           configuredUrls={configuredPreviewUrls}
-          visible
+          visible={rightPanelOpen}
           onSendAnnotation={(annotation, image) => {
             void onSend(undefined, "foreground", { annotation, image });
           }}
